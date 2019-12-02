2f0acfd4bed76143113d37638c87258f87a2d0fe
fabioventu <venturini.fabio@iisgalvanimi.edu.it>
2019-12-02 20:59:14 +0000

    punto 2

--- venturini/src/app/app.module.ts
+++ venturini/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { App2Component } from './app2/app2.component';
+import { App3Component } from './app3/app3.component';
+import { App4Component } from './app4/app4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    App2Component,
+    App3Component,
+    App4Component
   ],
   imports: [
     BrowserModule
